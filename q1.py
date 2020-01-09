1c52477c61c1e1bcbb4627ec7f5cc57570417f77
solusion <59156859+wuguohaoECE@users.noreply.github.com>
2020-01-09 21:33:41 +0800

    Delete q1.py

--- q1.py
+++
@@ -1,6 +0,0 @@
-def myfunction(n):
-    """test whether the input number is a positive odd integer. Don't worry about the incorrect input.
-    """
-    ### BEGIN SOLUTION
-    return (n>0)&(n%2==1)
-    ### END SOLUTION
